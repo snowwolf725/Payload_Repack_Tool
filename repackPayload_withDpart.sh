26cda8e0a6cf1608cb7c435e631c313e2b253608
SnowWolf725 <snowwolf725@gmail.com>
2024-12-23 21:34:04 +0800

    update config file for Oneplus12

--- repackPayload_withDpart.sh
+++ repackPayload_withDpart.sh
@@ -22,7 +22,7 @@ name_list=$(find "$folder" -type f -exec basename {} \; | sed 's/\.[^.]*$//' | p
 
 file_list=$(find "$folder" -type f | paste -sd ":" -)
 
-./bin/delta_generator_$arch -out_file=output/unsigned-payload.bin -partition_names=$name_list -new_partitions=$file_list -dynamic_partition_info_file=./META/dynamic_partitions_info.txt -new_postinstall_config_file=./META/postinstall_config.txt
+./bin/delta_generator_$arch -out_file=output/unsigned-payload.bin -partition_names=$name_list -new_partitions=$file_list -dynamic_partition_info_file=./META/dynamic_partitions_info.txt 
 
 ./bin/delta_generator_$arch --in_file=output/unsigned-payload.bin -signature_size=256 -out_metadata_hash_file=output/sig_metadata.bin -out_hash_file=output/sig_hash.bin
 
